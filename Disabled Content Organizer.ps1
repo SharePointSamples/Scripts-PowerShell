e6ab1e4ba0d806dd90aefbdbef94900a1e87b8eb
Gesilene Martins <gesilene@live.com>
2015-04-26 22:31:02 -0300

    Content Organizer
    Script que desabilita o recurso Content Organizer de uma biblioteca

---
+++ Disabled Content Organizer.ps1
@@ -0,0 +1,13 @@
+﻿if((Get-PSSnapin | Where {$_.Name -eq "Microsoft.SharePoint.PowerShell"}) -eq $null) {
+ Add-PSSnapin Microsoft.SharePoint.PowerShell;
+}
+
+Get-SPSite -Identity http://server/site | Get-SPWeb -Limit ALL | ForEach-Object { 
+  Disable-SPFeature –Identity DocumentRouting –url $_.Url –Confirm:$false
+  $dropOffLibrary = $_.Lists["Library"]
+  $dropOffLibrary.AllowDeletion = "True"
+  $dropOffLibrary.Update()
+  $dropOffLibrary.Delete()
+}
+
+PAUSE
