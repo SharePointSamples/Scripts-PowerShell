f8ee430871beb5cdaf95f04f75ffa11952d90d8c
Gesilene Martins <gesilene@live.com>
2015-04-26 22:47:44 -0300

    Rename Get Groups and users.ps1 to GetGroupsAndUsers.ps1

--- GetGroupsAndUsers.ps1
+++ GetGroupsAndUsers.ps1
@@ -1,4 +1,4 @@
-$site = Get-SPSite <Endereço do Site>
+$site = Get-SPSite <EndereÃ§o do Site>
 $groups = $site.RootWeb.sitegroups
 foreach ($grp in $groups) {$grp.name + " - " + $grp.Users.Count; foreach ($user in $grp.users) {"   " + $user.name} }
 $site.Dispose() 
